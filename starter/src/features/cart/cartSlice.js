2f8850320450d95da137267744b0a7bc69574d3c
Mgphone <51868037+Mgphone@users.noreply.github.com>
2023-06-08 04:53:28 +0100

    asyncthunk

--- starter/src/features/cart/cartSlice.js
+++ starter/src/features/cart/cartSlice.js
@@ -1,11 +1,21 @@
-import { createSlice } from "@reduxjs/toolkit";
-import cartItems from "../../cartItems";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+// import cartItems from "../../cartItems";
+const url = "https://course-api.com/react-useReducer-cart-project";
+
 const initialState = {
-  cartItems: cartItems,
+  cartItems: [],
   amount: 0,
   total: 0,
   isLoading: true,
 };
+export const getCartItems = createAsyncThunk("cart/getCartItems", async () => {
+  try {
+    const response = await fetch(url);
+    return await response.json();
+  } catch (err) {
+    return console.error();
+  }
+});
 const cartSlice = createSlice({
   name: "cart",
   initialState,
@@ -35,6 +45,19 @@ const cartSlice = createSlice({
       });
       state.amount = amount;
       state.total = total.toFixed(2);
+    },
+  },
+  extraReducers: {
+    [getCartItems.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [getCartItems.fulfilled]: (state, action) => {
+      // console.log(action);
+      state.isLoading = false;
+      state.cartItems = action.payload;
+    },
+    [getCartItems.rejected]: (state) => {
+      state.isLoading = false;
     },
   },
 });
